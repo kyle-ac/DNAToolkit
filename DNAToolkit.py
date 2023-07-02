ec524239a5012d4a2b88dffaf21005dfaa2358dc
Kyle Craninx <kcraninx@gmail.com>
2023-07-02 09:18:09 +0200

    Integration of all ORFs with Translates
    Added function that generated different possible reading frames and the proteins that can be produced from each. Checked with real seq NCBI Reference Sequence: NM_000207.3

--- DNAToolkit.py
+++ DNAToolkit.py
@@ -1,5 +1,5 @@
 # DNA Toolkit file 
-import collections
+from collections import Counter
 from Sequences import  *
 
 # Check the Sequence to make sure  it is a DNA String 
@@ -46,3 +46,70 @@ def gc_content_subsec(seq, k=20):
         subseq = seq[i:i + k]
         res.append(gc_content(subseq))
     return res
+
+def translate_seq(seq, init_pos = 0):
+    """Tranlsates a DNA sequence into its Amino acid sequence"""
+    return [DNA_Codons [seq[pos:pos + 3]] for pos in range (init_pos, len(seq) - 2, 3)] 
+
+def codon_usage (seq, AminoAcid):
+    """Provides the frequency of each codon encoding a given amino acid in a DNA sequence"""
+    tmpList= []
+    for i in range (0, len(seq)-2, 3):
+        if DNA_Codons[seq[i:i+3]] == AminoAcid: 
+            tmpList.append(seq[i:i+3])
+
+    freqDict = dict(Counter(tmpList))
+    totalWeight = sum(freqDict.values())
+    for seq in freqDict:
+        freqDict[seq] = round (freqDict[seq]/totalWeight, 2)
+    return freqDict
+
+def gen_reading_frames(seq):
+    """Generate the six reading frames of a DNA sequences, including the reverse complement"""
+    frames = []
+    frames.append(translate_seq(seq, 0))
+    frames.append(translate_seq(seq, 1))
+    frames.append(translate_seq(seq, 2))
+    frames. append(translate_seq(reverse_complement(seq), 0))
+    frames. append(translate_seq(reverse_complement(seq), 1))
+    frames. append(translate_seq(reverse_complement(seq), 2))
+
+    return frames 
+
+def proteins_from_rf (aa_seq):
+    """Compute all possible proteins in an amino acid seq and return a list of possible proteins"""
+    current_prot = []
+    proteins = []
+    for aa in aa_seq:
+        if aa == "_":
+            #STOP adding AA if stop codon is read 
+            if current_prot:
+                for p in current_prot:
+                    proteins.append(p)
+                current_prot = []
+        else: 
+            # Start adding AA if M - Start Codon is read 
+            if aa == "M":
+                current_prot.append("")
+            for i in range (len(current_prot)):
+                current_prot[i] += aa
+    return proteins
+        
+def all_proteins_from_orfs (seq, startReadPos=0, endReadPos = 0, ordered = False):
+    """Compute all possible proteins from all open reading frames """
+    """Protein search DB: https://www.ncbi.nlm.nih.gov/nuccore/NM_001185097.2  """
+    """API can be used to pull protein info """
+    if endReadPos > startReadPos:
+        rfs = gen_reading_frames(seq[startRead: endRead])
+    else:
+        rfs = gen_reading_frames(seq)
+
+    res =[]    
+    for rf in rfs: 
+        prots = proteins_from_rf(rf)
+        for p in prots:
+            res.append(p)
+
+    if ordered: 
+        return sorted(res, key=len, reverse = True )
+    return res 
--- main.py
+++ main.py
@@ -10,16 +10,29 @@ rndDNAStr =  ''.join([random.choice(Nucleotides)
 
 DNAStr = validateSeq(rndDNAStr)
 
-print(f'\n Sequence: {coloured(DNAStr)}\n')
-print(f'[1] Sequence Length: {len(DNAStr)}\n')
-print(coloured(f'[2] Nucleotide Frequency: {countNucFrequency(DNAStr)}\n'))
+# print(f'\n Sequence: {coloured(DNAStr)}\n')
+# print(f'[1] Sequence Length: {len(DNAStr)}\n')
+# print(f'[2] Nucleotide Frequency: {countNucFrequency(DNAStr)}\n')
 
-print(f'[3] DNA/RNA Transcription: {transcription(DNAStr)}\n')
+# # print(f'[3] DNA/RNA Transcription: {transcription(DNAStr)}\n')
 
-# Printing DNA Pattern 5'->3' with reverse complement
-print(f"[4] DNA String + Complement + Reverse Complement:\n5'{coloured(DNAStr)} 3'")
-print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
-print(f"5' {reverse_complement(DNAStr)} 3' [Rev. Complement]\n")
-print(f'[5] GC Content: {gc_content(DNAStr)}%\n')
-print(
-    f'[6] + GC Content in Subsection k = 5: {gc_content_subsec(DNAStr, k = 5)}\n')
+# # # Printing DNA Pattern 5'->3' with reverse complement
+# print(f"[4] DNA String + Complement + Reverse Complement:\n5'{coloured(DNAStr)} 3'")
+# print(f"3' {reverse_complement(DNAStr)[::-1]} 5' [Complement]")
+# print(f"5' {reverse_complement(DNAStr)} 3' [Rev. Complement]\n")
+
+# # # Analysing the Sequence 
+# print(f'[5] GC Content: {gc_content(DNAStr)}%\n')
+# print(f'[6] GC Content in Subsection k = 5: {gc_content_subsec(DNAStr, k = 5)}\n')
+# print(f'[7] Amino Acid Sequence: {translate_seq(DNAStr, 0)}\n')
+# print(f'[8] Codon Freqency (L): {codon_usage(DNAStr, "L")}\n')
+
+# # # Finding proteins by reading frames
+# print('[9] Reading Frames: ')
+# for frame in gen_reading_frames(DNAStr): 
+#     print (frame)
+
+# Protein search within reading frames 
+print('\n[10] All proteins in 6 ORFs: ')
+for prot in all_proteins_from_orfs(NM_000207_3, 0, 0, True ):
+    print(f'{prot}')
